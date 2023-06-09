910b8e1d8191c8cc4f9a9fd1fdb161a400f788f7
Jeff Fredenburg <jfreden@fnal.gov>
2023-06-09 11:03:22 -0500

    created file to setup VNC sessions remotely

---
+++ bin/setup-vnc.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# parse the command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --username=*)
+            username="${1#*=}"
+            shift
+            ;;
+        --hostname=*)
+            echo $1
+            echo ${}
+            hostname="${1#*=}"
+            shift
+            ;;
+        --address=*)
+            address="${1#*=}"
+            shift
+            ;;
+        *)
+            echo "Unknown option $1"
+            shift
+            ;;
+    esac
+done
+
+username=${username:?"Error. username not define"}
+hostname=${hostname:?"Error. hostname not define"}
+address=${address:?"Error. address not define"}
+
+(
+    set -x
+    kinit -ft ~/.kerberos/${username}.keytab ${username}@FNAL.GOV && klist
+    ssh ${username}@${hostname}.fnal.gov "vncserver :${address} -geometry 2560x1440 -localhost ; vncserver -list"
+)
