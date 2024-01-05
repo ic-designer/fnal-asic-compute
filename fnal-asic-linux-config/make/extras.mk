dee951a1299518a89b9318cbd2f5172fb7aac279
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-05 10:04:01 -0600

    created initial configuration for linux

---
+++ fnal-asic-linux-config/make/extras.mk
@@ -0,0 +1,54 @@
+# define build-bash-library
+# 	@echo 'Bulding library $@'
+# 	@cat $^ > $@
+# endef
+
+
+# define build-bash-executable
+# 	@echo 'Bulding executable $@'
+# 	@echo '#!/usr/bin/env bash\n' > $@
+# 	@cat $^ >> $@
+# 	@echo >> $@
+# 	@echo 'if [[ $${BASH_SOURCE[0]} == $${0} ]]; then' >> $@
+# 	@echo '    ('  >> $@
+# 	@echo '        set -euo pipefail'  >> $@
+# 	@echo '        $(strip $(1)) "$$@"'  >> $@
+#     @echo '    )'  >> $@
+# 	@echo 'fi'  >> $@
+# endef
+
+
+
+define git-clone-shallow
+	$(if $(wildcard $2), rm -rf $2)
+	@git clone -qv --depth=1 $1 $2 $(if $3, --branch $3)
+endef
+
+
+define git-list-remotes
+	@for d in $(1)/*; do \
+		echo '$${d} $$(git -C $${d} remote get-url origin --push)'; \
+	done
+endef
+
+
+define install-as-file
+	@install -dv $(dir $@)
+	install -v $< $@
+	@test -f $@
+	@diff $@ $<
+endef
+
+
+define install-as-link
+	@install -dv $(dir $@)
+	@ln -sfv $(realpath $<) $@
+	@test -L $@
+	@test -f $@
+	@diff $@ $<
+endef
+
+
+.PRECIOUS: %/.
+%/. :
+	@mkdir -p $@
---
+++ fnal-asic-linux-config/make/private.mk
@@ -0,0 +1,80 @@
+# Required Paths
+DESTDIR ?= $(error ERROR: Undefined variable DESTDIR)
+HOMEDIR ?= $(error ERROR: Undefined variable HOMEDIR)
+NAME ?= $(error ERROR: Undefined variable NAME)
+VERSION ?= $(error ERROR: Undefined variable VERSION)
+
+LIBSUBDIR ?= $(error ERROR: Undefined variable LIBSUBDIR)
+WORKDIR ?= $(error ERROR: Undefined variable WORKDIR)
+WORKDIR_PKGS ?= $(error ERROR: Undefined variable WORKDIR_PKGS)
+WORKDIR_TEST ?= $(error ERROR: Undefined variable WORKDIR_TEST)
+
+override PKGSUBDIR:=$(LIBSUBDIR)/$(NAME)/$(NAME)-$(VERSION)
+
+
+# Includes
+include make/extras.mk
+
+
+# Private targets
+.PHONY: private_install
+private_install: \
+		$(DESTDIR)/$(PKGSUBDIR)/.bashrc \
+		$(DESTDIR)/$(PKGSUBDIR)/.bashrc_profile \
+		$(DESTDIR)/$(PKGSUBDIR)/.local/bin/filename-search-and-replace \
+		$(DESTDIR)/$(PKGSUBDIR)/.ssh/config \
+		$(HOMEDIR)/.bashrc \
+		$(HOMEDIR)/.bashrc_profile \
+		$(HOMEDIR)/.local/bin/filename-search-and-replace \
+		$(HOMEDIR)/.ssh/config
+
+$(DESTDIR)/$(PKGSUBDIR)/% : %
+ifeq ($(VERSION),develop)
+	$(call install-as-link)
+else
+	$(call install-as-file)
+endif
+
+$(HOMEDIR)/% : $(DESTDIR)/$(PKGSUBDIR)/%
+	$(call install-as-link)
+
+
+.PHONY: private_uninstall
+private_uninstall:
+	@-\rm -fv $(DESTDIR)/$(PKGSUBDIR)/*
+	@-\rm -dv $(DESTDIR)/$(PKGSUBDIR)
+	@-\rm -dv $(dir $(DESTDIR)/$(PKGSUBDIR))
+	@-\rm -dv $(DESTDIR)/$(LIBSUBDIR)
+	@test ! -e $(DESTDIR)/$(PKGSUBDIR)
+	@-\rm -dv $(HOMEDIR)/.bashrc
+	@test ! -e $(HOMEDIR)/.bashrc
+	@-\rm -dv $(HOMEDIR)/.bashrc_profile
+	@test ! -e $(HOMEDIR)/.bashrc_profile
+	@-\rm -dv $(HOMEDIR)/.ssh/config
+	@test ! -e $(HOMEDIR)/.ssh/config
+	@-\rm -dv $(HOMEDIR)/.local/bin/filename-search-and-replace
+	@test ! -e $(HOMEDIR)/.local/bin/filename-search-and-replace
+
+
+.PHONY: private_test
+private_test :
+	@$(MAKE) install DESTDIR=$(abspath $(WORKDIR_TEST)) HOMEDIR=$(abspath $(WORKDIR_TEST))/home
+
+
+.PHONY: private_clean
+private_clean:
+	@echo "Cleaning directories: $(WORKDIR), $(WORKDIR_PKGS)"
+	$(if $(wildcard $(WORKDIR)), rm -rf $(WORKDIR))
+	$(if $(wildcard $(WORKDIR_PKGS)), rm -rf $(WORKDIR_PKGS))
+
+
+.PHONY: private_pkg_list
+private_pkg_list:
+	$(call git-list-remotes, $(WORKDIR_PKGS))
+
+
+.PHONY: private_pkg_override
+private_pkg_override: REPO ?= $(error ERROR: Repo not defined. Please defined REPO=<repository>)
+private_pkg_override: NAME ?= $(error ERROR: Name not defined. Please defined NAME=<repository>)
+private_pkg_override:
+	$(call git-clone-shallow, $(REPO), $(WORKDIR_PKGS)/$(NAME))
